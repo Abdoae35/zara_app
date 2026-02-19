5233279760615da4f521a34e57ae0f54d0626ba4
Abdoae35 <156617861+Abdoae35@users.noreply.github.com>
2026-02-19 05:02:25 +0200

    hoodiesComplete

--- lib/features/home/widgets/list_view_section.dart
+++ lib/features/home/widgets/list_view_section.dart
@@ -3,75 +3,25 @@ import 'package:flutter_svg/svg.dart';
 import 'package:zara_app/core/constants/app_assets.dart';
 import 'package:zara_app/core/constants/app_fonts.dart';
 import 'package:zara_app/core/styles/app_colors.dart';
+import 'package:zara_app/features/home/widgets/card_view.dart';
 
 class listViewSection extends StatelessWidget {
-  const listViewSection({
-    super.key,
-  });
+  const listViewSection({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(left: 24),
       child: SizedBox(
-        height: 281,
+        height: 300,
         child: ListView.separated(
           scrollDirection: Axis.horizontal,
           itemCount: 20,
           itemBuilder: (context, index) {
-            return Container(
-              width: 200,
-              decoration: BoxDecoration(
-                color: AppColors.inputBackgroundColor,
-    
-                borderRadius: BorderRadius.circular(8),
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Stack(
-                    children: [
-                      Center(
-                        child: Image.asset(
-                          'assets/images/MensHarringtonJacket.png',
-    
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                      Positioned(
-                        top: 9,
-                        right: 12,
-                        child: SvgPicture.asset(AppAssets.heart),
-                      ),
-                    ],
-                  ),
-    
-                  SizedBox(height: 9),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 4),
-                    child: Text(
-                      "Men's Harrington Jacket",
-                      style: TextStyle(
-                        fontFamily: AppFonts.circularStd,
-                        fontSize: 14,
-                      ),
-                    ),
-                  ),
-                  SizedBox(height: 6),
-    
-                  Padding(
-                    padding: const EdgeInsets.only(left: 4),
-                    child: Text(
-                      "\$148.00",
-                      style: TextStyle(
-                        fontFamily: AppFonts.gabarito,
-                        fontSize: 15,
-                        fontWeight: FontWeight.w700,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
+            return cardView(
+              path: AppAssets.MensHarringtonJacket,
+              label: 'Men\'s Harrington Jacket',
+              price: '\$148.00',
             );
           },
           separatorBuilder: (context, index) {
