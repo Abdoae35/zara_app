e781a85a1d146183bc086e1453f96e5c16124ba5
Eslam-Egy <eslamahmed2004.10@gmail.com>
2026-02-26 01:33:45 +0200

    Done

--- lib/features/product/view/product_page.dart
+++ lib/features/product/view/product_page.dart
@@ -19,248 +19,206 @@ class _ProductPageState extends State<ProductPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(24, 8, 24, 16),
+          child: SizedBox(
+            width: double.infinity,
+            height: 52,
+            child: ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: AppColors.primaryColor,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: const [
+                  Text("\$148", style: TextStyle(color: Colors.white)),
+                  Text("Add to Bag", style: TextStyle(color: Colors.white)),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
       body: SingleChildScrollView(
         child: Column(
           children: [
-            SizedBox(height: 61),
+            const SizedBox(height: 61),
+
             Padding(
               padding: const EdgeInsets.all(24.0),
               child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   containricons(icon: Icons.chevron_left),
-                  SizedBox(width: 262),
                   containricons(icon: Icons.favorite_border),
                 ],
               ),
             ),
 
+            
             SizedBox(
               height: 248,
-
               child: ListView.builder(
                 scrollDirection: Axis.horizontal,
                 itemCount: image.length,
                 itemBuilder: (context, index) {
                   return Padding(
-                    padding: const EdgeInsetsGeometry.fromLTRB(24, 24, 0, 0),
-
+                    padding: const EdgeInsets.fromLTRB(24, 24, 0, 0),
                     child: Image.asset(image[index], fit: BoxFit.cover),
                   );
                 },
               ),
             ),
+
+           
             Padding(
               padding: const EdgeInsets.all(24.0),
               child: Column(
-                children: [
-                  Row(children: [Text("Men's Harrington Jacket")]),
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: const [
+                  Text("Men's Harrington Jacket"),
                   SizedBox(height: 15),
-                  Row(children: [Text("\$148")]),
+                  Text("\$148"),
                 ],
               ),
             ),
 
-            SizedBox(height: 9),
+            const SizedBox(height: 9),
 
-            Padding(
-              padding: const EdgeInsetsGeometry.fromLTRB(24, 0, 24, 0),
-              child: Container(
-                width: 342,
-                height: 56,
-                decoration: BoxDecoration(
-                  color: AppColors.backgroundBlur,
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                child: Row(
-                  children: [
-                    SizedBox(width: 16),
-                    Text("Size"),
-                    SizedBox(width: 211),
-                    Text("S"),
-                    SizedBox(width: 29),
-                    Icon(Icons.keyboard_arrow_down),
-                  ],
-                ),
-              ),
-            ),
-            SizedBox(height: 12),
-
-            Padding(
-              padding: const EdgeInsetsGeometry.fromLTRB(24, 0, 24, 0),
-              child: Container(
-                width: 342,
-                height: 56,
-                decoration: BoxDecoration(
-                  color: AppColors.backgroundBlur,
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                child: Row(
-                  children: [
-                    SizedBox(width: 16),
-                    Text("color"),
-                    SizedBox(width: 199),
-                    Container(
-                      width: 16,
-                      height: 16,
-                      decoration: BoxDecoration(
-                        color: Color(0xffB3B68B),
-                        borderRadius: BorderRadius.circular(30),
-                      ),
-                    ),
-                    SizedBox(width: 29),
-                    Icon(Icons.keyboard_arrow_down),
-                  ],
-                ),
-              ),
-            ),
-            SizedBox(height: 12),
-            Padding(
-              padding: const EdgeInsetsGeometry.fromLTRB(24, 0, 24, 0),
-              child: Container(
-                width: 342,
-                height: 56,
-                decoration: BoxDecoration(
-                  color: AppColors.backgroundBlur,
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                child: Row(
-                  children: [
-                    SizedBox(width: 16),
-                    Text("Quantity"),
-                    SizedBox(width: 105),
-                    Container(
-                      width: 40,
-                      height: 40,
-                      decoration: BoxDecoration(
-                        color: AppColors.primaryColor,
-                        borderRadius: BorderRadius.circular(30),
-                      ),
-                      child: Icon(
-                        Icons.add,
-                        color: AppColors.inputBackgroundColor,
-                      ),
-                    ),
-
-                    SizedBox(width: 23),
-                    Text("1"),
-                    SizedBox(width: 23),
-                    Container(
-                      width: 40,
-                      height: 40,
-                      decoration: BoxDecoration(
-                        color: AppColors.primaryColor,
-                        borderRadius: BorderRadius.circular(30),
-                      ),
-                      child: Icon(
-                        Icons.remove,
-                        color: AppColors.inputBackgroundColor,
-                      ),
-                    ),
-                  ],
-                ),
+            
+            _infoContainer(
+              child: Row(
+                children: const [
+                  SizedBox(width: 16),
+                  Text("Size"),
+                  Spacer(),
+                  Text("S"),
+                  SizedBox(width: 12),
+                  Icon(Icons.keyboard_arrow_down),
+                  SizedBox(width: 16),
+                ],
               ),
             ),
 
-            Padding(
-              padding: const EdgeInsetsGeometry.fromLTRB(24, 26, 24, 75),
-              child: Column(
+            const SizedBox(height: 12),
+
+            _infoContainer(
+              child: Row(
                 children: [
-                  Row(
-                    children: [
-                      Text(
-                        "Built for life and made to last, this full-zip corduroy jacket is",
-                        style: TextStyle(
-                          fontWeight: FontWeight.w400,
-                          fontSize: 12,
-                        ),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      Text(
-                        "part of our Nike Life collection. The spacious fit gives you ",
-                        style: TextStyle(
-                          fontWeight: FontWeight.w400,
-                          fontSize: 12,
-                        ),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      Text(
-                        "plenty of room to layer underneath, while the soft corduroy",
-                        style: TextStyle(
-                          fontWeight: FontWeight.w400,
-                          fontSize: 12,
-                        ),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      Text(
-                        "keeps it casual and timeless.",
-                        style: TextStyle(
-                          fontWeight: FontWeight.w400,
-                          fontSize: 12,
-                        ),
-                      ),
-                    ],
-                  ),
-
-                  SizedBox(height: 24),
-                  Column(
-                    children: [
-                      Row(children: [Text("Shipping & Returns")]),
-                      SizedBox(height: 12),
-                      Row(
-                        children: [
-                          Text(
-                            "Free standard shipping and free 60-day returns",
-                          ),
-                        ],
-                      ),
-                      SizedBox(height: 24),
-                      Row(children: [Text("Reviews")]),
-                    ],
-                  ),
-                  SizedBox(height: 12),
-                  Row(children: [Text("4.5 Ratings")]),
-                  SizedBox(height: 12),
-                  Row(children: [Text("213 Reviews")]),
-                  SizedBox(height: 16),
-                  RatingContainar(image: "assets/images/Ellipse 15.png"),
-                  SizedBox(height: 12),
-                  RatingContainar(image: "assets/images/Ellipse 15 (1).png"),
-                  SizedBox(height: 75),
-
-                  SizedBox(
-                    width: double.infinity,
-                    height: 52,
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: AppColors.primaryColor,
-                      ),
-                      child: Row(
-                        children: [
-                          Text("\$148", style: TextStyle(color: Colors.white)),
-                          SizedBox(width: 181),
-                          Text(
-                            "Add to Bag",
-                            style: TextStyle(color: Colors.white),
-                          ),
-                        ],
-                      ),
+                  const SizedBox(width: 16),
+                  const Text("Color"),
+                  const Spacer(),
+                  Container(
+                    width: 16,
+                    height: 16,
+                    decoration: BoxDecoration(
+                      color: const Color(0xffB3B68B),
+                      borderRadius: BorderRadius.circular(30),
                     ),
                   ),
+                  const SizedBox(width: 12),
+                  const Icon(Icons.keyboard_arrow_down),
+                  const SizedBox(width: 16),
+                ],
+              ),
+            ),
+
+            const SizedBox(height: 12),
+
+            _infoContainer(
+              child: Row(
+                children: [
+                  const SizedBox(width: 16),
+                  const Text("Quantity"),
+                  const Spacer(),
+                  _circleBtn(Icons.add),
+                  const SizedBox(width: 23),
+                  const Text("1"),
+                  const SizedBox(width: 23),
+                  _circleBtn(Icons.remove),
+                  const SizedBox(width: 16),
+                ],
+              ),
+            ),
+
+            Padding(
+              padding: const EdgeInsets.fromLTRB(24, 26, 24, 75),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    "Built for life and made to last, this full-zip corduroy jacket is",
+                    style: TextStyle(fontSize: 12),
+                  ),
+                  const Text(
+                    "part of our Nike Life collection. The spacious fit gives you",
+                    style: TextStyle(fontSize: 12),
+                  ),
+                  const Text(
+                    "plenty of room to layer underneath, while the soft corduroy",
+                    style: TextStyle(fontSize: 12),
+                  ),
+                  const Text(
+                    "keeps it casual and timeless.",
+                    style: TextStyle(fontSize: 12),
+                  ),
+
+                  const SizedBox(height: 24),
+                  const Text("Shipping & Returns"),
+                  const SizedBox(height: 12),
+                  const Text("Free standard shipping and free 60-day returns"),
+                  const SizedBox(height: 24),
+                  const Text("Reviews"),
+                  const SizedBox(height: 12),
+                  const Text("4.5 Ratings"),
+                  const SizedBox(height: 12),
+                  const Text("213 Reviews"),
+                  const SizedBox(height: 16),
+
+                  const RatingContainar(image: "assets/images/Ellipse 15.png"),
+                  const SizedBox(height: 12),
+                  const RatingContainar(
+                    image: "assets/images/Ellipse 15 (1).png",
+                  ),
+
+                  const SizedBox(height: 75),
                 ],
               ),
             ),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget _circleBtn(IconData icon) {
+    return Container(
+      width: 40,
+      height: 40,
+      decoration: BoxDecoration(
+        color: AppColors.primaryColor,
+        borderRadius: BorderRadius.circular(30),
+      ),
+      child: Icon(icon, color: AppColors.inputBackgroundColor),
+    );
+  }
+
+  Widget _infoContainer({required Widget child}) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24),
+      child: Container(
+        width: 342,
+        height: 56,
+        decoration: BoxDecoration(
+          color: AppColors.backgroundBlur,
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: child,
       ),
     );
   }
@@ -273,6 +231,7 @@ class RatingContainar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Row(
           children: [
@@ -284,23 +243,22 @@ class RatingContainar extends StatelessWidget {
               ),
               child: Image.asset(image),
             ),
-            SizedBox(width: 12),
-            Text("Alex Morgan"),
-
-            SizedBox(width: 100),
+            const SizedBox(width: 12),
+            const Text("Alex Morgan"),
+            const Spacer(),
             Icon(Icons.star, color: AppColors.primaryColor),
             Icon(Icons.star, color: AppColors.primaryColor),
             Icon(Icons.star, color: AppColors.primaryColor),
-            Icon(Icons.star, color: Color(0xffF4F4F4)),
-            Icon(Icons.star, color: Color(0xffF4F4F4)),
+            const Icon(Icons.star, color: Color(0xffF4F4F4)),
+            const Icon(Icons.star, color: Color(0xffF4F4F4)),
           ],
         ),
-        SizedBox(height: 4),
-        Text(
-          "Gucci transcribes its heritage, creativity, and innovation into a \nplenitude of collections. From staple items to distinctive \naccessories.",
+        const SizedBox(height: 4),
+        const Text(
+          "Gucci transcribes its heritage, creativity, and innovation into a plenitude of collections.",
         ),
-        SizedBox(height: 4),
-        Row(children: [Text("12days ago")]),
+        const SizedBox(height: 4),
+        const Text("12days ago"),
       ],
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:zara_app/core/styles/app_colors.dart';
 import 'package:zara_app/features/main/main_app_screen.dart';
+import 'package:zara_app/features/product/view/product_page.dart';
 
 void main() {
   runApp(const MainApp());
@@ -14,7 +15,7 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData(scaffoldBackgroundColor: AppColors.backgroundColor),
       debugShowCheckedModeBanner: false,
-      home: MainAppScreen(),
+      home: ProductPage(),
     );
   }
 }
