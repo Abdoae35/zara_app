2f26989092da54c987b7513774f0071624755704
Abdoae35 <156617861+Abdoae35@users.noreply.github.com>
2026-02-26 04:07:21 +0200

    EndBottomSheet

--- lib/core/constants/app_assets.dart
+++ lib/core/constants/app_assets.dart
@@ -52,4 +52,5 @@ class AppAssets {
   static const String item2 = "assets/images/item2.png";
   static const String item3 = "assets/images/item3.png";
   static const String item4 = "assets/images/item4.png";
+  static const String truee = "assets/icons/true.svg";
 }
--- lib/features/search&filter/widgets/filter.dart
+++ lib/features/search&filter/widgets/filter.dart
@@ -4,12 +4,15 @@ import 'package:zara_app/core/constants/app_assets.dart';
 import 'package:zara_app/core/constants/app_fonts.dart';
 import 'package:zara_app/core/styles/app_colors.dart';
 
-class filter extends StatelessWidget {
+class filter extends StatefulWidget {
   final String label;
   final String? prefixIcon;
   final String? suffexIcon;
   final Color backGroundColor;
   final Color textColor;
+  final List<String> dataList;
+  final String labelBottomSheet;
+
   const filter({
     super.key,
     required this.label,
@@ -17,44 +20,152 @@ class filter extends StatelessWidget {
     this.suffexIcon,
     required this.backGroundColor,
     required this.textColor,
+    required this.dataList,
+    required this.labelBottomSheet,
   });
+
+  @override
+  State<filter> createState() => _filterState();
+}
+
+class _filterState extends State<filter> {
+  int selectedIndex = -1;
+
+  bool isSeleted = false;
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {
         showModalBottomSheet(
-          backgroundColor: AppColors.backgroundBlur,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadiusGeometry.only(
-              topLeft: Radius.circular(20),
-              topRight: Radius.circular(20),
-            ),
-          ),
+          backgroundColor: AppColors.backgroundColor,
           context: context,
           builder: (context) {
-            return SizedBox(
-              width: double.infinity,
-              child: Column(children: [Text('onSale')]),
+            return StatefulBuilder(
+              builder: (context, setModalState) {
+                return SizedBox(
+                  width: double.infinity,
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 26,
+                          vertical: 20,
+                        ),
+                        child: Row(
+                          mainAxisAlignment: .spaceBetween,
+                          children: [
+                            GestureDetector(
+                              onTap: () {
+                                setModalState(() {
+                                  selectedIndex = -1;
+                                });
+                              },
+                              child: Text(
+                                'Clear',
+                                style: TextStyle(
+                                  color: AppColors.blackColor,
+                                  fontFamily: AppFonts.circularStd,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                            ),
+                            Text(
+                              widget.labelBottomSheet,
+                              style: TextStyle(
+                                fontFamily: AppFonts.gabarito,
+                                fontSize: 24,
+                                fontWeight: FontWeight.w700,
+                              ),
+                            ),
+                            CloseButton(color: AppColors.blackColor),
+                          ],
+                        ),
+                      ),
+
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 24),
+                        child: Column(
+                          children: [
+                            SizedBox(
+                              height: 400,
+                              child: ListView.separated(
+                                itemCount: widget.dataList.length,
+                                itemBuilder: (context, index) {
+                                  final isSelected = selectedIndex == index;
+                                  return GestureDetector(
+                                    onTap: () {
+                                      setModalState(() {
+                                        selectedIndex = index;
+                                      });
+                                    },
+                                    child: Container(
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(35),
+                                        color: isSelected
+                                            ? AppColors.primaryColor
+                                            : Color(0xffF4F4F4),
+                                      ),
+                                      child: Padding(
+                                        padding: const EdgeInsets.symmetric(
+                                          horizontal: 34,
+                                          vertical: 19,
+                                        ),
+                                        child: Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.spaceBetween,
+                                          children: [
+                                            Text(
+                                              widget.dataList[index],
+                                              style: TextStyle(
+                                                color: isSelected
+                                                    ? AppColors.backgroundColor
+                                                    : AppColors.blackColor,
+                                                fontSize: 16,
+                                                fontFamily:
+                                                    AppFonts.circularStd,
+                                                fontWeight: FontWeight.w500,
+                                              ),
+                                            ),
+                                            if (isSelected)
+                                              SvgPicture.asset(AppAssets.truee),
+                                          ],
+                                        ),
+                                      ),
+                                    ),
+                                  );
+                                },
+                                separatorBuilder: (context, index) =>
+                                    SizedBox(height: 16),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              },
             );
           },
         );
       },
       child: Container(
         decoration: BoxDecoration(
-          color: backGroundColor,
+          color: widget.backGroundColor,
           borderRadius: BorderRadius.circular(19),
         ),
         child: Padding(
           padding: const EdgeInsets.all(8.0),
           child: Row(
             children: [
-              SvgPicture.asset(prefixIcon ?? '', height: 25),
+              SvgPicture.asset(widget.prefixIcon ?? '', height: 25),
               SizedBox(width: 4),
               Text(
-                label,
+                widget.label,
                 style: TextStyle(
-                  color: textColor,
+                  color: widget.textColor,
                   fontFamily: AppFonts.circularStd,
                   fontSize: 16,
                 ),
@@ -62,9 +173,12 @@ class filter extends StatelessWidget {
               SizedBox(width: 5),
 
               SvgPicture.asset(
-                suffexIcon ?? '',
+                widget.suffexIcon ?? '',
                 height: 13,
-                colorFilter: ColorFilter.mode(textColor, BlendMode.srcIn),
+                colorFilter: ColorFilter.mode(
+                  widget.textColor,
+                  BlendMode.srcIn,
+                ),
               ),
             ],
           ),
